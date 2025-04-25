7f6f3f8d8e0378417373ca1abdc973801c425600
EmiHernandez <pintoemiliano02@gmail.com>
2025-04-24 21:37:45 -0600

    Actualización del proyecto con nuevos cambios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:18.1.0")
     implementation ("com.google.android.gms:play-services-location:21.0.1")
 
+    implementation ("androidx.biometric:biometric:1.2.0-alpha04")
+
 
 
 }
